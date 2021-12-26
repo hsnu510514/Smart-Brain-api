f7d72cd95dbe633b632972817b916dca64ec6bda
hsnu510514 <36807153+hsnu510514@users.noreply.github.com>
2021-12-26 11:04:58 +0800

    Update server.js

--- server.js
+++ server.js
@@ -20,6 +20,24 @@ const db = knex({
 const app = express();
 
 app.use(cors());
+app.use(function (req, res, next) {
+
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', 'https://ztm-smart-brain.herokuapp.com/');
+
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
+
+    // Set to true if you need the website to include cookies in the requests sent
+    // to the API (e.g. in case you use sessions)
+    res.setHeader('Access-Control-Allow-Credentials', true);
+
+    // Pass to next layer of middleware
+    next();
+});
 app.use(bodyParser.json());
 
 // app.get('/', (req, res) => { res.send(database.user) })
